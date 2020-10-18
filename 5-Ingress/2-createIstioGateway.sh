9e87730523b242ded9a6b56e9d8bb1600fe4cc38
Brandon Neo <brandon.neo@gmail.com>
2020-10-19 00:28:38 +1100

    Update 2-createIstioGateway.sh

--- 5-Ingress/2-createIstioGateway.sh
+++ 5-Ingress/2-createIstioGateway.sh
@@ -4,6 +4,8 @@ kubectl apply -f ../utils/sampleapp/ingress/istioGatewayRules.yaml
 
 export INGRESS_URL=$(kubectl describe svc istio-ingressgateway -n istio-system | grep "LoadBalancer Ingress:" | sed 's~LoadBalancer Ingress:[ \t]*~~')
 
+kubectl -n istio-system get service istio-ingressgateway -o jsonpath='{.spec.ports[?(@.name=="http2")].nodePort}'
+
 echo "----------------------------------------------------"
 echo "Gateway is running at : http://$INGRESS_URL"
 echo "----------------------------------------------------"

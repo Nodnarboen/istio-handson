e89c493b7cd85db7df9d859bee0ad783aa485f6a
Brandon Neo <brandon.neo@gmail.com>
2020-10-19 00:30:39 +1100

    Update 2-createIstioGateway.sh

--- 5-Ingress/2-createIstioGateway.sh
+++ 5-Ingress/2-createIstioGateway.sh
@@ -2,6 +2,23 @@
 
 kubectl apply -f ../utils/sampleapp/ingress/istioGatewayRules.yaml
 
+if hash gcloud 2>/dev/null; then
+    echo "Google Cloud"
+    export PROVIDER=GKE
+    export INGRESS_URL=$(kubectl describe svc istio-ingressgateway -n istio-system | grep "LoadBalancer Ingress:" | sed 's~LoadBalancer Ingress:[ \t]*~~')
+elif hash microk8s 2>/dev/null; then
+    echo "Microk8s"
+    export PROVIDER=Microk8s
+    export PUBLIC_IP=$(curl -s ifconfig.me) 
+    PUBLIC_IP_AS_DOM=$(echo $PUBLIC_IP | sed 's~\.~-~g')
+    export DOMAIN="${PUBLIC_IP_AS_DOM}.nip.io"
+    export INGRESS_PORT=$(kubectl -n istio-system get service istio-ingressgateway -o jsonpath='{.spec.ports[?(@.name=="http2")].nodePort}')
+    export INGRESS_URL=$DOMAIN:$INGRESS_PORT
+else
+    echo "No supported Provider (GCP or Microk8s) detected."
+    exit 1;
+fi
+
 export INGRESS_URL=$(kubectl describe svc istio-ingressgateway -n istio-system | grep "LoadBalancer Ingress:" | sed 's~LoadBalancer Ingress:[ \t]*~~')
 
 kubectl -n istio-system get service istio-ingressgateway -o jsonpath='{.spec.ports[?(@.name=="http2")].nodePort}'

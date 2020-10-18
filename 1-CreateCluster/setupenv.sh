a61140b0c7ccf8a1541f5826ae62a664247fe0d4
Brandon <student@ip-172-31-20-231.ap-southeast-2.compute.internal>
2020-10-18 22:42:22 +0000

    Latest

---
+++ 1-CreateCluster/setupenv.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+    echo ""
+    
+    echo "Creating GKE Cluster..."
+
+    export USER=$(gcloud auth list --filter=status:ACTIVE --format="value(account)" | awk -F"@" '{print $1}' | sed 's/\./-/g')
+    export CLUSTER="sebootcamp-istio"
+    export CLUSTER_NAME=${USER}-${CLUSTER}
+
+    export RAND=$(( $RANDOM % 3 + 1 ))
+
+    case $RAND in
+        1)
+        export ZONE="asia-southeast1-a"
+        ;;
+        2)
+        export ZONE="asia-southeast2-b"
+        ;;
+        3)
+        export ZONE="australia-southeast1-a"
+        ;;
+    esac
+
+    gcloud services enable container.googleapis.com
+
+    gcloud container clusters create $CLUSTER_NAME --zone=$ZONE --num-nodes=2 --machine-type=n1-highmem-2 --image-type=Ubuntu
+
+    kubectl create clusterrolebinding cluster-admin-binding --clusterrole=cluster-admin --user=$(gcloud config get-value account)
+
+echo "Cluster created"
+
